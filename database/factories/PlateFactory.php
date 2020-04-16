71b959a1f52054ca95997fda9fe5001b34b15807
Sofia Carballo <sofia.carballo@live.com>
2020-04-16 17:43:13 +0200

    Initial commit

---
+++ database/factories/PlateFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Plate;
+use Faker\Generator as Faker;
+
+$factory->define(Plate::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_04_16_090759_create_plates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plates');
+    }
+}
